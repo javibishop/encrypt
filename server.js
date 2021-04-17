aaea1f5d461c46a407f3e87fbcb75caac2cd7583
Javier <javibishop@gmail.com>
2021-04-17 10:12:14 -0300

    x

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 //para q funcione en herocu
 const cripto = require('./cryotohelper');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3010;
 
 /*middleware es un callback q se ejecuta siempre. */
 
